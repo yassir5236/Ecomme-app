8442f2089cb60199ca5c28a9584f6343a9b255aa
yassir5236 <yassiraitlahmadi@gmail.com>
2025-02-04 13:30:24 +0100

    fetching data from a fake store api

--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -10,10 +10,19 @@ import { ProductCardComponent } from "./product-card/product-card.component";
 })
 export class ProductListComponent {
 
+  async ngOnInit() {
+
+    const res = await  fetch('https://fakestoreapi.com/products');
+    const data = await res.json();
+    this.products.set(data);
+
+    
+  }
+
   products = signal<Product[]>([
-    { id: 1, title: 'Product 1', image:  "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg", price: 100 , stock:0},
-    { id: 2, title: 'Product 2', image:  "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg", price: 200 , stock:10},
-    { id: 3, title: 'Product 3', image:  "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg", price: 300 , stock:15}
+    // { id: 1, title: 'Product 1', image:  "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg", price: 100 , stock:0},
+    // { id: 2, title: 'Product 2', image:  "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg", price: 200 , stock:10},
+    // { id: 3, title: 'Product 3', image:  "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg", price: 300 , stock:15}
   ]);
 
 }
